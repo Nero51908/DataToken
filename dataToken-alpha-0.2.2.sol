a05f730a2048b877a72048f618d14cf966140407
Nero Liu <zhuzibajia@gmail.com>
2017-12-03 23:49:06 +0800

    basic functions for new contract variables are implemented

--- dataToken-alpha-0.2.2.sol
+++ dataToken-alpha-0.2.2.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.18;
 
-contract DataTokenAlpha{
+contract DataTokenAlpha {
     //////////////////////////////////////
     //////////////////////////////////////
     //Contract variables
@@ -9,13 +9,13 @@ contract DataTokenAlpha{
     uint public v2weiRate = 1;
     address[] providers;
     userInfo[] users;
-    struct userInfo{
-        State Identification;
+    struct userInfo {
         address etherAddress;
         address provider;
         uint256 dataUsage;
         address[] receivers;
     }
+    mapping (address=>State) public Identification;
     mapping (address=>uint256) public tokenBalance;
     mapping (address=>uint256) public providerIndex;//if Identification == PROVIDER, this should be a unique value (i.e. a non-zero value)
     mapping (address=>uint256) public userIndex;//if Identification != NOTCONTRACTUSER, this should be a unique value (i.e. a non-zero value)
@@ -39,10 +39,12 @@ contract DataTokenAlpha{
     function DataTokenAlpha() public {
         //initialize index 0 in Info array
         userIndex[this] = users.length;//index of the element created by push operation is array length naturally
-        users.push(userInfo(State.ISRECEIVER,this,0,0,new address[](0x0)));
+        users.push(userInfo(this,0,0,new address[](0x0)));
+        Identification[this] = State.ISRECEIVER;
         //manually creating a default provider for testing purpose
-        userIndex[0xdd870fa1b7c4700f2bd7f44238821c26f7392148]= users.length;
-        users.push(userInfo(State.ISPROVIDER,0xdd870fa1b7c4700f2bd7f44238821c26f7392148,0x0,0,new address[](0x0)));
+        userIndex[0xdd870fa1b7c4700f2bd7f44238821c26f7392148] = users.length;
+        users.push(userInfo(0xdd870fa1b7c4700f2bd7f44238821c26f7392148,0x0,0,new address[](0x0)));
+        Identification[0xdd870fa1b7c4700f2bd7f44238821c26f7392148] = State.ISPROVIDER;
         providerIndex[0xdd870fa1b7c4700f2bd7f44238821c26f7392148] = providers.length;
         providers.push(0xdd870fa1b7c4700f2bd7f44238821c26f7392148);
     }
@@ -53,52 +55,65 @@ contract DataTokenAlpha{
     ////////////////////////////////////////
     //Modifiers
     ////////////////////////////////////////
+    modifier isContractUser() {
+        if(Identification[msg.sender] != State.NOTCONTRACTUSER)
+        {
+            _;
+        }
+    }
+    modifier isGoodUser() {//user identification value is not HAVENOTPAID
+        if(Identification[msg.sender] == State.ISPROVIDER || Identification[msg.sender] == State.ISRECEIVER)
+        {
+            _;
+        }
+    }
         //
         //isNotContractUser modifier: to check if the user is currently not a user of DataTokenAlpha
         //
-        modifier isNotContractUser() {
-        if(users[userIndex[msg.sender]].Identification == 0){
-            if(userIndex[msg.sender] != 0){
+        modifier isNotContractUser() {//this modifie will recover user identification of a new user who was a contract user
+        if(Identification[msg.sender] == State.NOTCONTRACTUSER){
+            if(userIndex[msg.sender] != 0){//"was a user" case
                 modifierResult("This address was a user of DataToken", userIndex[msg.sender]);
-                users[userIndex[msg.sender]].Identification = ISRECEIVER;
+                Identification[msg.sender] = State.ISRECEIVER;
                 functionCallResult("Contract user identity is recovered, welcome back.");
-            } else {
+            } else {//"completely new user" case
                 _;
             }
-        }else{
-            modifierResult("This address has been registered",users[userIndex[msg.sender]].Identification);
+        }else {
+            //"currently is a user" case
+            modifierResult("This address has been registered",uint8(Identification[msg.sender]));
         }
         }
         //
         //isProvider check, to be more readable, the name is "isNotReceiver"
         //
         modifier isProvider() {
-            if (users[userIndex[msg.sender]].Identification == 1){
+            if (Identification[msg.sender] == State.ISPROVIDER){
                 _;
             } else {
-                modifierResult("The caller is not a provider",users[userIndex[msg.sender]].Identification);
+                modifierResult("The caller is not a provider",uint8(Identification[msg.sender]));
             }
         }
         //
         //isReceiver check
         //
         modifier isReceiver() {
-            if (users[userIndex[msg.sender]].Identification == 2){
-                modifierResult("The caller is a receiver.");
+            if (uint8(Identification[msg.sender]) == 2){
+                modifierResult("The caller is a receiver.",uint8(Identification[msg.sender]));
                 _;    
             } else {
-                modifierResult("The caller is not a receiver");
+                modifierResult("The caller is not a receiver",uint8(Identification[msg.sender]));
             }
         }
         //
         //haveNotPaid check
         //
         modifier haveNotPaid() {
-            if (users[userIndex[msg.sender]].Identification == 3){
-                modifierResult("The caller have not paid last bill", users[userIndex[msg.sender]].Identification);
+            if (Identification[msg.sender] == State.HAVENOTPAID){
+                modifierResult("The caller have not paid last bill", uint8(Identification[msg.sender]));
                 _;
-            }else{
-                modifierResult("The caller have paid last bill", users[userIndex[msg.sender]].Identification);
+            }else {
+                modifierResult("The caller have paid last bill", uint8(Identification[msg.sender]));
             }
         }
     ////////////////////////////////////////
@@ -107,67 +122,97 @@ contract DataTokenAlpha{
 
 
     //add current message sender to user list if it is not in the list.
+    //a special case that the message sender was a contract user and has suspended it's user identify in DataTOken Contract
+    //the modifier isNotContractUser will detect such kind of ex-users and recover their contract user identity when they call addUser() function
     function addUser()
     isNotContractUser
     public
     {
-        userIndex[msg.sender] = users.length;
-        users.push(userInfo(State.ISRECEIVER,msg.sender,0,0,new address[](0x0))));
-        
+        userIndex[msg.sender] = users.length;//the unique index for this user.
+        users.push(userInfo(msg.sender,0,0,new address[](0x0)));//initialize user information
+        Identification[msg.sender] = State.ISRECEIVER;
+        functionCallResult("New user is created.");
     }
     //remove current message sender from the user list if it were in it.
-    function removeUser() isUser public{
-        isNotNew[msg.sender]=false;
-        userOperationResult("The user is marked as not using this contract.");
+    function removeUser() 
+    isReceiver
+    public
+    {
+        Identification[msg.sender] = State.NOTCONTRACTUSER;
+        functionCallResult("Suspend: The user is marked as not using this contract.");
     }
-    
-    //
+    //switch user identification value to ISPROVIDER
+    function suProvider() 
+    isReceiver
+    public
+    {
+        //was a provider case
+        if(providerIndex[msg.sender] != 0){
+            providers[providerIndex[msg.sender]] = msg.sender;
+            Identification[msg.sender] = State.ISPROVIDER;
+            functionCallResult("You have succeessfully turned to be a provider.");
+        }else {
+            //the first time to be a provider case
+            providerIndex[msg.sender] = providers.length;
+            providers.push(msg.sender);
+            Identification[msg.sender] = State.ISPROVIDER;
+            functionCallResult("You have succeessfully turned to be a provider.");
+        }
+    }
+    function suReceiver()
+    isProvider
+    public
+    {
+        providers[providerIndex[msg.sender]] = 0x0;//remove address from providers(address[]) array
+        Identification[msg.sender] = State.ISRECEIVER;
+        functionCallResult("You have succeessfully turned to be a receiver.");  
+    }
     //topUp function: sender must input a non-zero value (Ether)
     //current setting: 1 Ether = 1000 token
-    function topUp() isUser payable public {
-        Info[index[msg.sender]].tokenBalance+=msg.value;
-        //event log 
-        userBalanceUpdate("Exchanged this many of token from Ether: ",Info[index[msg.sender]].tokenBalance);
+    function topUp() 
+    isContractUser 
+    payable 
+    public 
+    {
+        tokenBalance[msg.sender] += (msg.value / v2weiRate);
+        tokenBalanceUpdate("Exchanged this many of token from Ether: ",tokenBalance[msg.sender]);
     }
     
     //
     //withdraw Ether from this contract
     //redeem Ether using all token of current user.
     //msg.sender will pay for this send() operation, which is undertaken by Ethereum network.
-    function withdraw() isUser public {
-        if(msg.sender.send(Info[index[msg.sender]].tokenBalance)){
-            userBalanceUpdate("Withdraw operation has succeeded, redeemed amount of wei:", Info[index[msg.sender]].tokenBalance);
-            Info[index[msg.sender]].tokenBalance=0;
+    function withdraw() 
+    isGoodUser 
+    public 
+    {
+        if(msg.sender.send(tokenBalance[msg.sender])){
+            tokenBalanceUpdate("Withdraw operation has succeeded, redeemed amount of wei:", tokenBalance[msg.sender] * v2weiRate);
+            delete tokenBalance[msg.sender];
         } else {
             revert();
-            userBalanceUpdate("Withdraw operation has faild and all changes are reverted",0);
+            tokenBalanceUpdate("Withdraw operation has faild and all changes are reverted", 0);
         }
     }
     
     //
     //function transfer token in side this contract
     //
-    function transfer(address _provider, address _receiver, uint256 _amount) isUser public {
-        if(Info[index[_receiver]].tokenBalance - _amount >= 0){
-        Info[index[_receiver]].tokenBalance -= _amount;
-        Info[index[_provider]].tokenBalance += _amount;
+    function transfer(address _sender, address _target, uint256 _amount)
+    isContractUser
+    public  
+    returns(bool)
+    {
+        if (tokenBalance[msg.sender] - _amount >= 0){
+        tokenBalance[_sender] -= _amount;
+        tokenBalance[_target] += _amount;
         //delete the value of provider after a complete payment
-        Info[index[_receiver]].provider=0;
-        //paid is true by default, so there is no need to change 
-        //event log
-        userBalanceUpdate("Token transfer from receiver to provider, amount:",_amount);
+        tokenBalanceUpdate("Token transfer finished, amount:",_amount);
+        return true;
         } else {
-            userBalanceUpdate("Token transfer failed, not enough token in receiver account, transfer value:",_amount);
-            Info[index[_provider]].paid = false;
+            tokenBalanceUpdate("Token transfer failed, not enough token in receiver account, token not transferred:",_amount);
+            return false;
         }
-    }
-    //
-    //Charge the data receiver after using.
-    //
-    //input of pay should be from userInfo (Info[]) of msg.sender
-    //there should be a interface of this contract to assign volume of msg.sender
-    function pay(address _provider, uint256 _volume) isReceiver public {
-        transfer(_provider, msg.sender, _volume*v2weiRate);
     }
    //
    //link the msg.sender to a provider in provider list
@@ -176,19 +221,40 @@ contract DataTokenAlpha{
    isReceiver 
    public
    {
-       for(uint i=0; i <= providers.length; i++)
+       //iteration to find a provier
+       for (uint i = 0; i <= providers.length; i++)
        {
-         if(Info[index[providers[i]]].receiver.length < 5 && providers[i] != 0){
-            Info[index[providers[i]]].receiver.push(msg.sender);
-            Info[index[msg.sender]].provider = providers[i];
-            userOperationResult("Provider and receiver are paired.");
+            //current version limit providers and reveivers are one on one pairs
+            if (users[userIndex[providers[i]]].receivers.length < 2 && providers[i] != 0x0) {
+            users[userIndex[providers[i]]].receivers.push(msg.sender);
+            users[userIndex[msg.sender]].provider = providers[i];
+            Identification[msg.sender] = State.HAVENOTPAID;
+            functionCallResult("Provider and receiver are paired.");
             break;
-         }
-         if(i==providers.length){
-            userOperationResult("No provider is available now.");
-         }
+            }
+            if (i==providers.length) {
+                functionCallResult("No provider is available now.");
+            }
        }
    }
+    //
+    //Receiver need to call pay function to make themselves ISRECEIVER again, otherwise 
+    //they will remain HAVENOTPAID Identification(Address=>State)
+    //
+    function pay() 
+    haveNotPaid
+    public 
+    {
+        if (transfer(msg.sender, users[userIndex[msg.sender]].provider, users[userIndex[msg.sender]].dataUsage))
+        {
+            users[userIndex[msg.sender]].dataUsage = 0;
+            Identification[msg.sender] = State.ISRECEIVER;
+            functionCallResult("Your bill is now paid.");
+        } else {
+            //payment did not succeed
+            functionCallResult("Payment did not succeed.");
+        }
+    }
 //////////////////////////////////////////////////
     //function for test
     function contractBalance() constant public returns (uint256 ) {
@@ -197,27 +263,10 @@ contract DataTokenAlpha{
     function giveToken(uint256 _amount) 
     public
     {
-        Info[index[msg.sender]].tokenBalance+=_amount;
-        userBalanceUpdate("This many token is given to current message sender: ",_amount);
+        tokenBalance[msg.sender] += _amount;
+        tokenBalanceUpdate("This many token is given to current message sender: ", _amount);
     }
-    function suProvider() 
-    isReceiver 
-    public
-    {
-        Info[index[msg.sender]].userRole = true;
-        providers.push(msg.sender);
-        pIndex[msg.sender] = providerIndx;
-        providerIndx += 1;
-        userOperationResult("You have succeessfully turned to be provider.");
-    }
-    function suReceiver()
-    isNotReceiver
-    public
-    {
-        Info[index[msg.sender]].userRole = false;
-        delete providers[pIndex[msg.sender]];
-        userOperationResult("You have succeessfully turned to be receiver.");  
-    }
+
     function chv2weiRate(uint _newRate) 
     public
         {
